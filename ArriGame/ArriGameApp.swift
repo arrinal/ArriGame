2c2645102dcc8cefd233c948365c059517bc73fc
Arrinal S <arrinalsf@gmail.com>
2021-09-11 14:38:28 +0700

    Initial Commit

---
+++ ArriGame/ArriGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArriGameApp.swift
+//  ArriGame
+//
+//  Created by Arrinal Sholifadliq on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ArriGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
